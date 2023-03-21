ec96edc44112f5f0ddbd01973d316437f3c561c8
Kaushik Mondal <kaushik@kaushiks-mbp.lan>
2023-03-20 23:48:02 -0400

    initialize react and firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import {useAuthState} from 'react-firebase-hooks'
+import {useCollectionData} from 'react-firebase-hooks/firestore' 
+
+firebase.initializeApp({
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+const [user] = useAuthState(auth)
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+  }
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>
+}
+
+function SignOut() {
+  return auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+}
+
+function ChatRoom() {
+  const messagesRef = firestore.collection('messages')
+  const query = messagesRef.orderBy('createdAt').limit(25)
+  const messages = useCollectionData(query, {idField: 'id'})
+  return(
+    <>
+      <div>{messages && messages.map()}</div>
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        
       </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
